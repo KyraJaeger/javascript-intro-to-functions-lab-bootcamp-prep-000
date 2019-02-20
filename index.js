837e0d519861ae5d2b3780209572b4ef5f21e66f
Kyra J <kyraejaeger@gmail.com>
2019-02-20 04:20:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function sayHiToGrandma(string) {
   if(string===string.toLowerCase()) {
     return "I can't hear you!"
   }
-  if(string.toUpperCase() === uppercase) {
-    console.log("YES INDEED!")
+  if(string===string.toUpperCase) {
+    return "YES INDEED!"
   }
   if(string==="I love you, Grandma.") {
-    console.log("I love you, too.")
+    return "I love you, too."
   }
 }

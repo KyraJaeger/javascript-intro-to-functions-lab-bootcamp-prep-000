9bcfaea38187f2c6dc8da3252d5f149d3cd52ffb
Kyra J <kyraejaeger@gmail.com>
2019-02-20 04:01:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function whisper(string) {
-  return string.toLowerCase()
+function logshout(string) {
+  console.log(string.toLowerCase())
 }

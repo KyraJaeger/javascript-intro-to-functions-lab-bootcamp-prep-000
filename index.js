90c52f1bf8b6ab8b4139c08d667cfb6c0078559b
Kyra J <kyraejaeger@gmail.com>
2019-02-20 04:13:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,19 +21,6 @@ function sayHiToGrandma(string) {
   if(string.toUpperCase() === uppercase){
     console.log("YES INDEED!")
   }
-  if(string)
-}
-
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
+  if(string === "I love you, Grandma."){
+    console.log("I love you, too.)
+  }
